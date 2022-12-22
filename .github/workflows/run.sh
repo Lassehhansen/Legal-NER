8db2ef601648e38c612bb9ddb5e020893e1ca400
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2022-12-22 22:20:48 +0100

    Creating run.sh file, for automation

---
+++ .github/workflows/run.sh
@@ -0,0 +1,8 @@
+#Installing required packages 
+
+python3 -m pip install --upgrade pip #Using python3 using module pipm upgrade pip
+python3 -m pip install -r ../../requirements.txt
+
+#Run the code from main.py with the given arugments 
+
+python main.py dommedump/ danish --padding --unknown
